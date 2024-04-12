f5a3d7b0818c227a5015ae7c93c4c09a48c9de3d
Tarek <tael3424@th-wildau.de>
2024-04-12 16:19:21 +0200

    addTask function added plus some css

--- script.js
+++ script.js
@@ -2,3 +2,39 @@
 const toDoInput = document.getElementById("todo-input");
 const addButton = document.getElementById("add-button");
 const toDoList = document.getElementById("todo-list");
+let taskNum = 0;
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+  if (toDoInput.value != "") {
+    console.log("Add Task");
+
+    let newDoneButton = document.createElement("button");
+    // newDoneButton.innerHTML = "Done";
+    let newTask = document.createElement("li");
+    let newTaskText = document.createElement("p");
+    newTaskText.textContent = toDoInput.value;
+    let newDeleteButton = document.createElement("button");
+    newDeleteButton.innerHTML = "Delete";
+
+    newTask.appendChild(newDoneButton);
+    newTask.appendChild(newTaskText);
+    newTask.appendChild(newDeleteButton);
+
+    toDoList.appendChild(newTask);
+    //ID hinzufügen
+    taskNum = toDoList.getElementsByTagName("li").length;
+    //set Attribute ID
+    newDoneButton.setAttribute("id", `doneButton${taskNum}`);
+    newTask.setAttribute("id", `task${taskNum}`);
+    newTaskText.setAttribute("id", `taskText${taskNum}`);
+    newDeleteButton.setAttribute("id", `deleteButton${taskNum}`);
+    //set Attribute Class
+    newDoneButton.setAttribute("class", `doneButton`);
+    newTask.setAttribute("class", `task`);
+    newTaskText.setAttribute("class", `taskText`);
+    newDeleteButton.setAttribute("class", `deleteButton`);
+  } else {
+    console.log("Input is empty");
+  }
+}
